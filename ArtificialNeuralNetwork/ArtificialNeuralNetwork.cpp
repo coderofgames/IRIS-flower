9ea6ae6eb08356c64913763b5c0cf53c02c26029
coderofgames <dave@discrete-hosting.co.uk>
2016-10-19 20:50:13 +0100

    2.4
    Added a linked neural network object and tested

--- ArtificialNeuralNetwork/ArtificialNeuralNetwork.cpp
+++ ArtificialNeuralNetwork/ArtificialNeuralNetwork.cpp
@@ -146,153 +146,480 @@ float SUM(matrix& source)
 
 namespace Vanilla_Layer
 {
-	class Layer
+	namespace Non_Lnked_Layer
 	{
-	public:
-		matrix neurons_;
-		matrix thetas_;
-		matrix weights_;
-
-		matrix delta_thetas_;
-		matrix delta_weights_;
-
-		matrix deltas_;
-
-		Layer(){}
-
-		Layer(unsigned int num_elements, unsigned int num_inputs)
+		class Layer
 		{
-			neurons_.create(1, num_elements);
-			weights_.create(num_inputs, num_elements);
-			thetas_.create(1, num_elements);
-			delta_weights_.create(num_inputs, num_elements);
-			delta_thetas_.create(1, num_elements);
-		}
+		public:
+			matrix neurons_;
+			matrix thetas_;
+			matrix weights_;
 
+			matrix delta_thetas_;
+			matrix delta_weights_;
 
+			matrix deltas_;
 
-		void init_random_sample_weights_iris()
-		{
-			for (int i = 0; i < weights_.NumRows(); i++)
+			Layer(){}
+
+			Layer(unsigned int num_elements, unsigned int num_inputs)
 			{
-				for (int j = 0; j < weights_.NumCols(); j++)
+				neurons_.create(1, num_elements);
+				weights_.create(num_inputs, num_elements);
+				thetas_.create(1, num_elements);
+				delta_weights_.create(num_inputs, num_elements);
+				delta_thetas_.create(1, num_elements);
+			}
+
+
+
+			void init_random_sample_weights_iris()
+			{
+				for (int i = 0; i < weights_.NumRows(); i++)
 				{
-					weights_(i, j) = RandomFloat(0, 1) / 5;
+					for (int j = 0; j < weights_.NumCols(); j++)
+					{
+						weights_(i, j) = RandomFloat(0, 1) / 5;
+					}
+				}
+
+				for (int i = 0; i < thetas_.NumRows(); i++)
+				{
+					for (int j = 0; j < thetas_.NumCols(); j++)
+					{
+						thetas_(i, j) = RandomFloat(0, 1) / 5;
+					}
 				}
 			}
 
-			for (int i = 0; i < thetas_.NumRows(); i++)
+
+
+			//1 7 ... 7 3   3 3
+
+			void FeedForward(matrix &input_matrix)
 			{
-				for (int j = 0; j < thetas_.NumCols(); j++)
-				{
-					thetas_(i, j) = RandomFloat(0, 1) / 5;
-				}
+				neurons_ = input_matrix * weights_ - thetas_;
+				sigmoid(neurons_, neurons_);
+#ifdef VERBOSE
+				PrintNeurons();
+#endif
 			}
-		}
 
+			void BackPropogate(matrix &next_layer_weights_, matrix &next_layer_delta_weights_)
+			{
+				sigmoid_deriv(deltas_, neurons_);
 
+				next_layer_weights_.transpose();
 
-		//1 7 ... 7 3   3 3
+				// OPERATOR PRECEDENCE! it is easy to forget what you are computing
+				deltas_ = deltas_ | (next_layer_delta_weights_ * next_layer_weights_);
 
-		void FeedForward(matrix &input_matrix)
-		{
-			neurons_ = input_matrix * weights_ - thetas_;
-			sigmoid(neurons_, neurons_);
-#ifdef VERBOSE
-				PrintNeurons();	
-#endif
-		}
-
-		void BackPropogate(matrix &next_layer_weights_, matrix &next_layer_delta_weights_)
-		{
-			sigmoid_deriv(deltas_, neurons_);
-
-			next_layer_weights_.transpose();
-
-			// OPERATOR PRECEDENCE! it is easy to forget what you are computing
-			deltas_ = deltas_ | (next_layer_delta_weights_ * next_layer_weights_);
-
-			next_layer_weights_.transpose();
-
-#ifdef VERBOSE
-			PrintDeltas();
-#endif
-
-		}
-
-		void BackPropogate_output(matrix& output_error)
-		{
-			sigmoid_deriv(deltas_, neurons_);
-
-			//	deltas_ = (deltas_)| output_error;
-
-			deltas_ = output_error; // THIS WORKS FOR THE IRIS CASE
+				next_layer_weights_.transpose();
 
 #ifdef VERBOSE
 				PrintDeltas();
 #endif
-		}
 
-		void ComputeWeightDeltas(matrix &input_matrix, float alpha, float beta)
+			}
+
+			void BackPropogate_output(matrix& output_error)
+			{
+				sigmoid_deriv(deltas_, neurons_);
+
+				//	deltas_ = (deltas_)| output_error;
+
+				deltas_ = output_error; // THIS WORKS FOR THE IRIS CASE
+
+#ifdef VERBOSE
+				PrintDeltas();
+#endif
+			}
+
+			void ComputeWeightDeltas(matrix &input_matrix, float alpha, float beta)
+			{
+				delta_weights_.transpose();
+
+				deltas_.transpose();
+
+				delta_weights_ = delta_weights_ * beta + deltas_* input_matrix * alpha;
+
+				delta_weights_.transpose();
+
+				deltas_.transpose();
+
+				delta_thetas_ = delta_thetas_ * beta + deltas_* (-1.0f) * alpha;
+
+#ifdef VERBOSE
+				PrintDeltaWeights();
+#endif
+			}
+
+
+			void UpdateWeights()
+			{
+				weights_ = weights_ + delta_weights_;// 
+
+				thetas_ = thetas_ + delta_thetas_;
+
+#ifdef VERBOSE
+				PrintWeights();
+#endif
+			}
+
+			void PrintNeurons()
+			{
+				cout << "Neurons" << endl;
+				neurons_.print(3);
+				cout << endl;
+			}
+
+			void PrintDeltas()
+			{
+				cout << "Deltas" << endl;
+				deltas_.print(3);
+			}
+			void PrintDeltaWeights()
+			{
+				cout << "Delta Weights" << endl;
+				delta_weights_.print(3);
+			}
+			void PrintDeltaThetas()
+			{
+				cout << "Delta Thetas" << endl;
+				delta_thetas_.print(3);
+			}
+
+			void PrintWeights()
+			{
+				cout << "Weights" << endl;
+				weights_.print(3);
+			}
+			};
+
+	}
+
+	namespace Linked_Layer
+	{
+		class Layer;
+
+		class LayerConnection
+		{
+		public:
+			LayerConnection(Layer* Prev, Layer* Next, int num_inputs, int num_outputs)
+			{
+				weights_.create(num_inputs, num_outputs);
+				delta_weights_.create(num_inputs, num_outputs);
+
+				prev = Prev;
+				next = Next;
+			}
+
+			void init_random_sample_weights_iris()
+			{
+				for (int i = 0; i < weights_.NumRows(); i++)
+				{
+					for (int j = 0; j < weights_.NumCols(); j++)
+					{
+						weights_(i, j) = RandomFloat(0, 1) / 5;
+					}
+				}
+			}
+
+			void Compute_Weight_Deltas(matrix &deltas_, float alpha, float beta);
+
+			void UpdateWeights();
+
+			Layer * prev;
+			Layer * next;
+
+			matrix weights_;
+			matrix delta_weights_;
+		};
+
+		
+
+		class Layer
+		{
+		public:
+			LayerConnection *connection_in;
+			LayerConnection *connection_out;
+
+			matrix neurons_;
+			matrix thetas_;
+			matrix weights_;
+
+			matrix delta_thetas_;
+			matrix delta_weights_;
+
+			matrix deltas_;
+
+			Layer(){}
+
+			Layer(unsigned int num_elements, unsigned int num_inputs)
+			{
+				neurons_.create(1, num_elements);
+				weights_.create(num_inputs, num_elements);
+				thetas_.create(1, num_elements);
+				delta_weights_.create(num_inputs, num_elements);
+				delta_thetas_.create(1, num_elements);
+			}
+
+			Layer(unsigned int num_elements, unsigned int num_inputs, LayerConnection *in, LayerConnection *out) 
+			{
+				connection_in = in;
+				connection_out = out;
+				 
+				neurons_.create(1, num_elements);
+				weights_.create(num_inputs, num_elements);
+				thetas_.create(1, num_elements);
+				delta_weights_.create(num_inputs, num_elements);
+				delta_thetas_.create(1, num_elements);
+			}
+
+			void init_random_sample_weights_iris()
+			{
+				for (int i = 0; i < weights_.NumRows(); i++)
+				{
+					for (int j = 0; j < weights_.NumCols(); j++)
+					{
+						weights_(i, j) = RandomFloat(0, 1) / 5;
+					}
+				}
+
+				for (int i = 0; i < thetas_.NumRows(); i++)
+				{
+					for (int j = 0; j < thetas_.NumCols(); j++)
+					{
+						thetas_(i, j) = RandomFloat(0, 1) / 5;
+					}
+				}
+			}
+
+			matrix& FeedForward(matrix &input_matrix)
+			{
+				neurons_ = input_matrix * connection_in->weights_ - thetas_;
+				//neurons_ = input_matrix * weights_ - thetas_;
+				sigmoid(neurons_, neurons_);
+
+				if (connection_out && connection_out->next )
+					return connection_out->next->FeedForward(neurons_);
+
+				return neurons_;
+#ifdef VERBOSE
+				PrintNeurons();
+#endif
+			}
+
+			void BackPropogate()
+			{
+				sigmoid_deriv(deltas_, neurons_);
+
+				//if ( connection_out )
+				
+				connection_out->weights_.transpose();
+				deltas_ = deltas_ | (connection_out->next->deltas_ * connection_out->weights_);
+				connection_out->weights_.transpose();
+				
+				if (connection_in && connection_in->prev)
+					connection_in->prev->BackPropogate();
+
+				//next_layer_weights_.transpose();
+
+				// OPERATOR PRECEDENCE! it is easy to forget what you are computing
+				//deltas_ = deltas_ | (next_layer_delta_weights_ * next_layer_weights_);
+
+				//next_layer_weights_.transpose();
+
+#ifdef VERBOSE
+				PrintDeltas();
+#endif
+
+			}
+
+			void BackPropogate_output(matrix& output_error)
+			{
+				sigmoid_deriv(deltas_, neurons_);
+
+				//	deltas_ = (deltas_)| output_error;
+
+				deltas_ = output_error; // THIS WORKS FOR THE IRIS CASE
+
+				if (connection_in && connection_in->prev)
+					connection_in->prev->BackPropogate();
+#ifdef VERBOSE
+				PrintDeltas();
+#endif
+			}
+
+			void ComputeWeightDeltas( float alpha, float beta)
+			{
+				if ( connection_in )
+					connection_in->Compute_Weight_Deltas(deltas_, alpha, beta);
+				//delta_weights_.transpose();
+
+				//deltas_.transpose();
+
+				//delta_weights_ = delta_weights_ * beta + deltas_* input_matrix * alpha;
+
+				//delta_weights_.transpose();
+
+				//deltas_.transpose();
+
+				//delta_thetas_ = delta_thetas_ * beta + deltas_* (-1.0f) * alpha;
+				delta_thetas_ = delta_thetas_ * beta + deltas_* (-1.0f) * alpha;
+
+
+#ifdef VERBOSE
+				PrintDeltaWeights();
+#endif
+			}
+
+
+			void UpdateWeights()
+			{
+				if (connection_in)
+					connection_in->UpdateWeights();
+
+				thetas_ = thetas_ + delta_thetas_;
+
+#ifdef VERBOSE
+				PrintWeights();
+#endif
+			}
+
+			void PrintNeurons()
+			{
+				cout << "Neurons" << endl;
+				neurons_.print(3);
+				cout << endl;
+			}
+
+			void PrintDeltas()
+			{
+				cout << "Deltas" << endl;
+				deltas_.print(3);
+			}
+			void PrintDeltaWeights()
+			{
+				cout << "Delta Weights" << endl;
+				delta_weights_.print(3);
+			}
+			void PrintDeltaThetas()
+			{
+				cout << "Delta Thetas" << endl;
+				delta_thetas_.print(3);
+			}
+
+			void PrintWeights()
+			{
+				cout << "Weights" << endl;
+				weights_.print(3);
+			}
+		};
+
+
+		void LayerConnection::Compute_Weight_Deltas(matrix &deltas_, float alpha, float beta)
 		{
 			delta_weights_.transpose();
 
 			deltas_.transpose();
 
-			delta_weights_ = delta_weights_ * beta + deltas_* input_matrix * alpha;
+			delta_weights_ = delta_weights_ * beta + deltas_* this->prev->neurons_ * alpha;
 
 			delta_weights_.transpose();
 
 			deltas_.transpose();
 
-			delta_thetas_ = delta_thetas_ * beta + deltas_* (-1.0f) * alpha;
 
-#ifdef VERBOSE
-			PrintDeltaWeights();
-#endif
+			if (prev)
+				prev->ComputeWeightDeltas(alpha, beta);
+
+
 		}
 
-
-		void UpdateWeights()
+		void LayerConnection::UpdateWeights()
 		{
 			weights_ = weights_ + delta_weights_;// 
 
-			thetas_ = thetas_ + delta_thetas_;
-
-#ifdef VERBOSE
-			PrintWeights();
-#endif
+			if (prev)
+				prev->UpdateWeights();
 		}
 
-		void PrintNeurons()
+		class NeuralNetwork
 		{
-			cout << "Neurons" << endl;
-			neurons_.print(3);
-			cout << endl;
-		}
+		public:
+			NeuralNetwork(){
+				input_layer = 0;
+			}
 
-		void PrintDeltas()
-		{
-			cout << "Deltas" << endl;
-			deltas_.print(3);
-		}
-		void PrintDeltaWeights()
-		{
-			cout << "Delta Weights" << endl;
-			delta_weights_.print(3);
-		}
-		void PrintDeltaThetas()
-		{
-			cout << "Delta Thetas" << endl;
-			delta_thetas_.print(3);
-		}
+			NeuralNetwork( vector<int> &layer_sizes ){
+				
 
-		void PrintWeights()
-		{
-			cout << "Weights" << endl;
-			weights_.print(3);
-		}
-	};
+				num_layers = layer_sizes.size();
+				if (num_layers > 0)
+				{
+					input_layer = new Layer*[num_layers];
+					
+					input_layer[0] = new Layer(layer_sizes[0], 1);
+					input_layer[0]->connection_in = 0;
+					for (int i = 1; i < num_layers; i++ )
+					{
+													// num elements  num outputs
+						input_layer[i] = new Layer( layer_sizes[i], layer_sizes[i-1] ) ;
+
+						input_layer[i]->connection_in = new LayerConnection( input_layer[i - 1],  // prev
+																			 input_layer[i],      // next (this one)
+																			 layer_sizes[i - 1], // num_inputs (nodes in prev)
+																			 layer_sizes[i]);    // num_outputs (nodes in this)
+
+						input_layer[i - 1]->connection_out = input_layer[i]->connection_in; // the last layers output connection is the input to this
+						
+					}
+
+					input_layer[num_layers - 1]->connection_out = 0; // or something ...
+
+					// initialize the weights
+					for (int i = 0; i < num_layers; i++)
+					{
+						if (input_layer[i]->connection_in)
+							input_layer[i]->connection_in->init_random_sample_weights_iris();
+					}
+
+				}
+			}
+
+			matrix& FeedForward(matrix& input)
+			{
+				input_layer[0]->neurons_ = input;
+				return input_layer[1]->FeedForward(input);
+			}
+
+			void BackPropagateErrors(matrix& errors)
+			{
+				input_layer[num_layers-1]->BackPropogate_output(errors);
+			}
+
+			void ComputeDeltas(float alpha, float beta)
+			{
+				input_layer[num_layers - 1]->ComputeWeightDeltas(alpha, beta);
+			}
+
+			void UpdateWeights()
+			{
+				input_layer[num_layers - 1]->UpdateWeights();
+			}
+
+			int num_layers=0;
+			Layer **input_layer;
+			
+		};
+	}
+	
+
+	
+	
+
+	
 }
 
 
@@ -481,6 +808,172 @@ matrix& QuadraticCostFunction(matrix& out, matrix& desired_outputs, matrix& outp
 
 
 
+
+void Compute_IRIS_data_version_2_(int num_iterations, CSV &iris_data, vector<int> &training_set, vector<int> &test_set, vector<int> &indexes)
+{
+	Timer timer;
+
+
+
+	int input_data_size = 1;
+	int num_inputs = 4;
+	int num_hidden = 8;
+	int num_hidden_2 = 3;
+	int num_outputs = 1;
+
+	// ==========================================
+	matrix input_matrix(1, num_inputs);
+
+
+	float output_error = 0.0f;
+
+
+
+	float alpha = 0.5f;
+	float beta = 0.05f;
+
+	float sum_squared_errors = 0.0f;
+
+	vector<int> layer_sizes;
+	layer_sizes.push_back(num_inputs);
+	layer_sizes.push_back(num_hidden);
+	layer_sizes.push_back(num_hidden);
+	layer_sizes.push_back(num_hidden_2);
+
+	Vanilla_Layer::Linked_Layer::NeuralNetwork *neuralNet = new Vanilla_Layer::Linked_Layer::NeuralNetwork(layer_sizes);
+
+
+
+
+
+
+
+
+	//	indexes
+	//vector< matrix > output_buffer;
+
+	cout << endl;
+	cout << "Training, please wait ..." << endl;
+	//return;
+	timer.Start();
+
+	//	Sleep(2000);
+
+	float last_sum_squared_errors = 0.0f;
+	int positive_error_delta_count = 0;
+	int negative_error_delta_count = 0;
+	int alternation_count = 0;
+
+
+
+	for (int p = 0; p < num_iterations; p++)
+	{
+
+		for (int q = 0; q < training_set.size(); q++)
+		{
+			// index remap the shuffled set to the original data
+			input_matrix(0, 0) = iris_data.iris_data[training_set[q]][0];
+			input_matrix(0, 1) = iris_data.iris_data[training_set[q]][1];
+			input_matrix(0, 2) = iris_data.iris_data[training_set[q]][2];
+			input_matrix(0, 3) = iris_data.iris_data[training_set[q]][3];
+
+			matrix expected(1, 3);
+			for (int p = 0; p < 3; p++)
+			{
+				if ((int)iris_data.iris_data[training_set[q]][4] == p)
+				{
+					expected(0, p) = 1.0;
+				}
+				else
+				{
+					expected(0, p) = 0.0;
+				}
+			}
+
+
+
+
+			// feed forward
+			
+
+			matrix output = neuralNet->FeedForward(input_matrix);
+			
+			matrix errors = expected - output;
+
+			for (int p = 0; p < errors.NumCols(); p++)
+			{
+				if (abs(errors(0, p)) > 0.2)
+				{
+					errors(0, p) = errors(0, p) * sigmoid_deriv(output(0, p));
+				}
+				else errors(0, p) = 0.0f;
+			}
+
+
+			// back propagate output
+			neuralNet->BackPropagateErrors(errors);
+
+
+			// weight deltas
+			neuralNet->ComputeDeltas(alpha, beta);
+
+
+
+			// update weights
+			neuralNet->UpdateWeights();
+
+		}
+
+
+	}
+	timer.Update();
+	timer.Stop();
+	cout << "Finished training, Total calculation performed in " << timer.GetTimeDelta() << " seconds" << endl;
+
+
+	sum_squared_errors = 0.0f; // used here to count the number of correct guesses
+
+	for (int q = 0; q < test_set.size(); q++)
+	{
+
+
+		input_matrix(0, 0) = iris_data.iris_data[test_set[q]][0];
+		input_matrix(0, 1) = iris_data.iris_data[test_set[q]][1];
+		input_matrix(0, 2) = iris_data.iris_data[test_set[q]][2];
+		input_matrix(0, 3) = iris_data.iris_data[test_set[q]][3];
+		//input_matrix(0, 2) = -1.0f; // bias is always -1
+
+
+
+		matrix test_output = neuralNet->FeedForward(input_matrix);
+
+		if (test_output.NumCols() != 3) cout << "should have more columns" << endl;
+
+		int actual_type = (int)iris_data.iris_data[test_set[q]][4];
+
+		int found_type = 0;
+
+		if ((test_output(0, 0) > 0.8) && (test_output(0, 1) < 0.2) && (test_output(0, 2) < 0.2))
+		{
+			found_type = 0;
+		}
+		if ((test_output(0, 0) < 0.2) && (test_output(0, 1) > 0.8) && (test_output(0, 2) < 0.2))
+		{
+			found_type = 1;
+		}
+		if ((test_output(0, 0) < 0.2) && (test_output(0, 1) < 0.2) && (test_output(0, 2)> 0.8))
+		{
+			found_type = 2;
+		}
+
+			cout << "Test Sample: " << q << ", Found Type: " << found_type << ", Actual Type: " << actual_type << endl;
+
+		if (found_type == actual_type) sum_squared_errors += 1.0f;
+	}
+
+	cout << "Finished Test, Total classified correctly of " << test_set.size() << " tested: " << (int)sum_squared_errors << endl;
+
+}
 
 
 // abs(expected - y)
@@ -836,50 +1329,60 @@ int main(int argc, char* argv[])
 
 	// shuffle the indexes to randomize the order of the data
 
-		std::random_shuffle(indexes.begin(), indexes.end());
+	std::random_shuffle(indexes.begin(), indexes.end());
 	
-		// compute the half size of the data set
-		int half_size = indexes.size() / 2;
+	// compute the half size of the data set
+	int half_size = indexes.size() / 2;
 
-		// create a vector of indexes for training
-		vector<int> training_set;
-		// create a vector of indexes for testing
-		vector< int > test_set;
+	// create a vector of indexes for training
+	vector<int> training_set;
+	// create a vector of indexes for testing
+	vector< int > test_set;
 
-		// store the first half of the indexes in the training set
-		// and the second half of the indexes in the test set
-		for (int i = 0; i < indexes.size(); i++)
+	// store the first half of the indexes in the training set
+	// and the second half of the indexes in the test set
+	for (int i = 0; i < indexes.size(); i++)
+	{
+		if (i < 100)
 		{
-			if (i < 100)
-			{
-				training_set.push_back(indexes[i]);
-			}
-			else
-			{
-				test_set.push_back(indexes[i]);
-			}
+			training_set.push_back(indexes[i]);
 		}
+		else
+		{
+			test_set.push_back(indexes[i]);
+		}
+	}
 
 
-		Compute_IRIS_data_version_1_< Optimized_Layer::Layer >(1000, iris_data, training_set, test_set, indexes);
+/*	Compute_IRIS_data_version_1_< Optimized_Layer::Layer >(1000, iris_data, training_set, test_set, indexes);
 
-		cout << "completed Deep calculation with 2 hidden layers (Optimized_Layer)" << endl;
+	cout << "completed Deep calculation with 2 hidden layers (Optimized_Layer)" << endl;
 
-		Compute_IRIS_data_version_0_< Optimized_Layer::Layer >(1000, iris_data, training_set, test_set, indexes);
+	Compute_IRIS_data_version_0_< Optimized_Layer::Layer >(1000, iris_data, training_set, test_set, indexes);
 
-		cout << "completed Shallow calculation (Optimized_Layer)" << endl;
-		cout << endl << endl;
+	cout << "completed Shallow calculation (Optimized_Layer)" << endl;
+	cout << endl << endl;
+
+	Compute_IRIS_data_version_1_< Vanilla_Layer::Non_Lnked_Layer::Layer >(1000, iris_data, training_set, test_set, indexes);
+
+	cout << "completed Deep calculation with 2 hidden layers (Vanilla)" << endl;
+	cout << endl << endl;
+*/
+	Compute_IRIS_data_version_0_< Vanilla_Layer::Non_Lnked_Layer::Layer >(1000, iris_data, training_set, test_set, indexes);
+
+	cout << "completed Shallow calculation (Vanilla)" << endl;
+
+	
 
 
 
-		Compute_IRIS_data_version_0_< Vanilla_Layer::Layer >(1000, iris_data, training_set, test_set, indexes);
+	
+	cout << "Testing linked layer neural net object" << endl;
 
-		cout << "completed Shallow calculation (Vanilla)" << endl;
-
-		Compute_IRIS_data_version_1_< Vanilla_Layer::Layer >(1000, iris_data, training_set, test_set, indexes);
-
-		cout << "completed Deep calculation with 2 hidden layers (Vanilla)" << endl;
-		cout << endl << endl;
+	Compute_IRIS_data_version_2_(500, iris_data, training_set, test_set, indexes);
+	cout << endl << endl;
+	cout << "Finished testing linked layer neural net object" << endl;
+	cout << endl << endl;
 	
 	return 0;
 }
